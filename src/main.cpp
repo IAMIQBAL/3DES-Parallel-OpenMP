1267a263c36b6eba99f4de3bdc28ed34dbd920ec
iamiqbal <muhammadiqbal2015@hotmail.com>
2021-12-16 00:07:36 +0500

    Parallel.h added

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,82 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Serial.h"
+// #include "Serial.h"
+#include "Parallel.h"
 
 using namespace std;
 
 int main(){
-    SerialDES *sd = new SerialDES(key);
-    vector<string> genKeys = sd->generateKeys();
-    // for (int i = 0; i < genKeys.size(); i++){
-    //     cout << genKeys[i] << endl;
+
+    // Serial
+
+    // SerialDES *sd = new SerialDES(key);
+    // vector<string> genKeys = sd->generateKeys();
+    string fName = "../tests/plaintext.txt";
+
+    vector<string> text = readFile(fName);
+    // cout << "Input: ";
+    // for (int i = 0; i < text.size(); i++){
+    //     cout << text[i] << endl;
+    // }
+    vector<string> cipherText;
+    string *cipher = new string[text.size()];
+    // for (int i = 0; i < text.size(); i++){
+    //     cipher[i] = sd->encrypt(text[i], genKeys);
     // }
 
-    vector<string> text = readFile("input.txt");
-    cout << "Input: ";
-    for (int i = 0; i < text.size(); i++){
-        cout << text[i] << endl;
-    }
-    vector<string> cipherText;
-    string cipher[text.size()];
-    for (int i = 0; i < text.size(); i++){
-        cipher[i] = sd->encrypt(text[i], genKeys);
-    }
+    // cout << "Encrypted: " << cipher << endl;
+    // for (int i = 0; i < text.size(); i++){
+    //     cipherText.push_back(cipher[i]);
+    // }
 
-    cout << "Encrypted: " << cipher << endl;
-    for (int i = 0; i < text.size(); i++){
-        cipherText.push_back(cipher[i]);
-    }
+    // writeFile("enc.txt", cipherText);
 
-    writeFile("enc.txt", cipherText);
+    // for (int i = 0; i < cipherText.size(); i++){
+	// 	cout << "Encrypted: " << cipherText[i] << endl;
+	// }
 
-    for (int i = 0; i < cipherText.size(); i++){
-		cout << "Encrypted: " << cipherText[i] << endl;
-	}
+    // reverse(genKeys.begin(), genKeys.end());
+    // string decrypted;
+    // string dec[cipherText.size()];
+    // for (int i = 0; i < cipherText.size(); i++){
+    //     dec[i] = sd->encrypt(cipherText[i], genKeys);
+    // }
 
-    reverse(genKeys.begin(), genKeys.end());
-    string decrypted;
-    string dec[cipherText.size()];
-    for (int i = 0; i < cipherText.size(); i++){
-        dec[i] = sd->encrypt(cipherText[i], genKeys);
-    }
+    // for (int i = 0; i < cipherText.size(); i++){
+    //     decrypted += dec[i];
+    // }
 
-    for (int i = 0; i < cipherText.size(); i++){
-        decrypted += dec[i];
-    }
+    // cout << "Decrypted: " << decrypted << endl;
 
-    cout << "Decrypted: " << decrypted << endl;
+    // Parallel
+
+    // Parallel *pd = new Parallel(key);
+    // vector<string> genKeys = pd->pGenerateKeys();
+
+	// double Et1 = omp_get_wtime();
+    // cipher = pd->pEncrypt(text, genKeys);
+	// double Et2 = omp_get_wtime();
+
+    // for (int i = 0; i < text.size(); i++){
+    //     cipherText.push_back(cipher[i]);
+    // }
+
+    // writeFile("enc.txt", cipherText);
+
+    // reverse(genKeys.begin(), genKeys.end());
+    // string decrypted;
+    // string *dec = new string[cipherText.size()];
+
+	// double Dt1 = omp_get_wtime();
+    // dec = pd->pEncrypt(cipherText, genKeys);
+	// double Dt2 = omp_get_wtime();
+
+    // for (int i = 0; i < cipherText.size(); i++){
+    //     decrypted += dec[i];
+    // }
+
+    // // cout << "Decrypted: " << decrypted << endl;
+	// cout << "Encryption Time taken:" << (Et2 - Et1) << "s" << endl;
+	// cout << "Decryption Time taken:" << (Dt2 - Dt1) << "s" << endl;
 }
